ba07d76456fe437db1ab54b8dbb64adbd623f551
hellozin <paul26375@gmail.com>
2019-10-30 07:06:40 +0900

    CONFIG: Axios 추가

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+const instance = axios.create({})
+
+Vue.prototype.$http = instance
 
 new Vue({
   router,
