ccd2a628f5558a6a917060b605164b3fdd3a4eb4
hellozin <paul26375@gmail.com>
2019-12-14 20:18:15 +0900

    내용 및 배열 변경

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,40 +3,38 @@
     <b-card-text style="white-space: pre-line">
       <h3 class="display-4 py-3">Contact Us</h3>
 
-      <b-card class="my-2">
-        <h3><font-awesome-icon icon="envelope"></font-awesome-icon> Mail</h3>
-        <hr>
-        <p>paul26375@gmail.com</p>
-      </b-card>
+      <b-card-group deck>
+        <b-card class="my-2">
+          <h3><font-awesome-icon icon="envelope"></font-awesome-icon> Mail</h3>
+          <hr>
+          <p>paul26375@gmail.com</p>
+        </b-card>
 
-      <b-card class="my-2">
-        <h3><font-awesome-icon :icon="['fab','github-square']"></font-awesome-icon> Github</h3>
-        <hr>
-        <table class="table table-sm table-borderless w-auto">
-          <tr>
-            <td class="font-weight-bold">API Server</td>
-            <td><b-link href="https://github.com/hellozin/recruit-jogbo">https://github.com/hellozin/recruit-jogbo</b-link></td>
-          </tr>
-          <tr>
-            <td class="font-weight-bold">Frontend Server</td>
-            <td><b-link href="https://github.com/hellozin/recruit-jogbo-frontend">https://github.com/hellozin/recruit-jogbo-frontend</b-link></td>
-          </tr>
-        </table>
-        <p>건의사항, 버그 Issue, Pull Request 환영</p>
-      </b-card>
+        <b-card class="my-2">
+          <h3><font-awesome-icon :icon="['fab','github-square']"></font-awesome-icon> Github</h3>
+          <hr>
+          <strong><b-link href="https://github.com/hellozin/recruit-jogbo">Backend Server Github</b-link></strong><br />
+          <strong><b-link href="https://github.com/hellozin/recruit-jogbo-frontend">Frontend Server Github</b-link></strong>
+          <p>건의사항, 버그 Issue, Pull Request 환영</p>
+        </b-card>
+      </b-card-group>
 
-      <b-card class="my-2">
-        <h3><font-awesome-icon icon="server"></font-awesome-icon> 데모 서버</h3>
-        <hr>
-        <p>링크 : 준비중</p>
-      </b-card>
+      <b-card-group deck>
+        <b-card class="my-2">
+          <h3><font-awesome-icon :icon="['fab','slack']"></font-awesome-icon> Slack</h3>
+          <hr>
+          <p>영남대학교 컴퓨터공학과 재학생과 졸업생을 위한 커뮤니티입니다. Recruit Jogbo의 새 글, 업데이트 알림을 받을 수 있습니다.</p>
+          <b-link href="https://join.slack.com/t/yuce-community/shared_invite/enQtODYwOTE3MTUwMzA2LTQ0Y2Q0OGMwNDNlMzRmMDVmNjE2YmViMDMzNzg3NjNmZDU4YzlkYWJhMGVlMTkwYjY3NjdhMjQzZmNiMTcwOGQ">소개 및 초대링크</b-link>
+        </b-card>
 
-      <b-card class="my-2">
-        <h3><font-awesome-icon :icon="['fab','slack']"></font-awesome-icon> Slack</h3>
-        <hr>
-        <p>새 글 알림 및 페이지에 대한 이야기나 간단한 질문을 위한 공간 (새 글 알림 준비중)</p>
-        <b-link href="https://join.slack.com/t/yuce-community/shared_invite/enQtODU1MDU5Mjc4OTI4LWVhNTBlMDMzZWQ1ZGI1YThmZmI2MGY1MDIyNTNjNWY4MDE3ZTI5ZWU4OGNmYTA5MGY1MzE2MWQ0ZjYyOWIyMDU">초대링크</b-link>
-      </b-card>
+        <b-card class="my-2">
+          <h3><font-awesome-icon icon="server"></font-awesome-icon> 데모 서버</h3>
+          <hr>
+          <p>링크 : 준비중</p>
+        </b-card>
+      </b-card-group>
+
+      
     </b-card-text>
   </b-card>
 </template>
