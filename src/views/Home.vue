65a9425284b32e949d701357ade9680ad6331bf2
hellozin <paul26375@gmail.com>
2019-12-14 21:44:26 +0900

    홈페이지 내용 수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,30 @@
 <template>
   <div>
     <div id="jumbotron" class="rounded my-3 pt-5 px-5 py-3">
-      <h1 class="display-3">Recruit Jogbo</h1>
-      <p class="lead">영남대학교 컴퓨터공학과</p>
+      <h1 class="">Recruit Jogbo</h1>
+      <p class="">영남대학교 컴퓨터공학과</p>
       <hr>
       <p>취업 후기와 노하우를 공유하는 공간입니다.</p>
       <p class="text-danger">
         <font-awesome-icon icon="exclamation-circle"></font-awesome-icon>
-        조금 더 솔직하고 자세한 후기를 위해 모든 자료는 외부 업로드, 유출을 자제해 주시기 바랍니다.
+        아래 공지사항을 꼭 확인해주세요!
       </p>
       <p class="text-danger">
         <font-awesome-icon icon="exclamation-circle"></font-awesome-icon>
-        아직은 테스트 기간입니다. 서버가 예고 없이 일시적으로 중단 될 수 있습니다.
+        서버가 예고 없이 일시적으로 중단 될 수 있습니다.
       </p>
     </div>
 
     <b-card no-body>
       <b-tabs card>
-        <b-tab title="공지사항" active>
-          <vue-embed-gist gist-id="5381eeafc7d38fd163bfbad778dd7f66" file="RecruitJogbo"></vue-embed-gist>
+        <b-tab title="주의사항" active>
+          <vue-embed-gist gist-id="cd239aee7b4aca8f3ea7353194bec817" file="recruitjogbo-alert"></vue-embed-gist>
+        </b-tab>
+        <b-tab title="공지사항">
+          <vue-embed-gist gist-id="5381eeafc7d38fd163bfbad778dd7f66" file="recruitjogbo-announcement"></vue-embed-gist>
+        </b-tab>
+        <b-tab title="업데이트 예정">
+          <vue-embed-gist gist-id="a290de50a3021ac5a3f2dea82dd98abf" file="recruitjogbo-update"></vue-embed-gist>
         </b-tab>
       </b-tabs>
     </b-card>
