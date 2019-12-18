1c3124dc41d8ea89a836f396aab174e068ec7a43
hellozin <paul26375@gmail.com>
2019-12-18 23:29:56 +0900

    초대 링크 변경

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -24,7 +24,7 @@
           <h3><font-awesome-icon :icon="['fab','slack']"></font-awesome-icon> Slack</h3>
           <hr>
           <p>영남대학교 컴퓨터공학과 재학생과 졸업생을 위한 커뮤니티입니다. Recruit Jogbo의 새 글, 업데이트 알림을 받을 수 있습니다.</p>
-          <b-link href="https://join.slack.com/t/yuce-community/shared_invite/enQtODYwOTE3MTUwMzA2LTQ0Y2Q0OGMwNDNlMzRmMDVmNjE2YmViMDMzNzg3NjNmZDU4YzlkYWJhMGVlMTkwYjY3NjdhMjQzZmNiMTcwOGQ">소개 및 초대링크</b-link>
+          <b-link href="https://www.notion.so/hellozin/995760ba5f924e9496b6e936801c2860">소개 및 초대링크</b-link>
         </b-card>
 
         <b-card class="my-2">
